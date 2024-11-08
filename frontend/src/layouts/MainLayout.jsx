deff3501d061b1b7cb280c841bf22f3bfc7ffdd4
marshall nickolauson <marshnickol101@gmail.com>
2024-11-08 11:29:43 -0500

    placeholder

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 const MainLayout = () => {
-  return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            {/* Sidebar */}
+            {/* Outlet - Might just be a component called "ContentScreen" with children passed down */}
+            <Footer />
+        </>
+    );
 };
 export default MainLayout;
