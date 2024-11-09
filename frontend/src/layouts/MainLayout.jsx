f07b62d2905be4f3cae95d1e01fe19f3a34fece5
marshall nickolauson <marshnickol101@gmail.com>
2024-11-08 20:10:29 -0500

    update: header outlined

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -7,6 +7,14 @@ const MainLayout = () => {
     return (
         <>
             <Header />
+            <div className="flex">
+                <div className="w-2/12 md:w-2/12">
+                    Sidebar
+                </div>
+                <div className="w-10/12 md:w-10/12">
+                    Content
+                </div>
+            </div>
             {/* Sidebar */}
             {/* Outlet - Might just be a component called "ContentScreen" with children passed down */}
             <Footer />
